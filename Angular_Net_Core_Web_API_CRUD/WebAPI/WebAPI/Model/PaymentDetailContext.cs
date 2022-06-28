424b6844941e149d27066ce6c2b87daa4760c811
Bharat Patil <Bharat.Patil@apiglobalsolutions.com>
2022-06-28 13:39:42 +0530

    added code

---
+++ Angular_Net_Core_Web_API_CRUD/WebAPI/WebAPI/Model/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options): base(options)
+        {
+
+        }
+
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
---
+++ angular2-weather-app-tut-01-setup/angular2-weather-app-tut-01-setup/dev/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <header>
+            <h1>Angular 2 Weather</h1>
+        </header>
+    `,
+})
+export class AppComponent {
+}
